4d55869f781406a8f886a539f0dfe1a1950fc6a3
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-03 15:26:19 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const copyButton = document.getElementById("copy");
-    const robotImage = document.getElementById("robot")
+    const robotImage = document.getElementById("robot");
+    const foundCopied = document.getElementById("no_found_msg_copied");
+    const txt = document.getElementById("enter_txt");
+
     copyButton.style.display = 'none'; // Garantir que o botão esteja invisível ao carregar a página
 
     // Função para mostrar o botão "Copiar"
     function showCopyButton() {
         copyButton.style.display = "block";
-        robotImage.style.display = "none"
+        robotImage.style.display = "none";
+        foundCopied.style.display = "none";
+        txt.style.display = "none";
     }
 
     // Função para esconder o botão "Copiar"
     function hideCopyButton() {
         copyButton.style.display = "none";
         robotImage.style.display = "block";
+        foundCopied.style.display = "block";
+        txt.style.display = "block";
+        foundCopied.innerText = "Texto copiado com sucesso!";
+        txt.innerText = "Utilize seu texto como quiser!";
     }
 
     // Adicionar evento ao botão "Copiar"
