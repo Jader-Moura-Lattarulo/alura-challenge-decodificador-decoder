c37bc1b038d2312edd2422c297ae77821e7f26e9
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-07-03 15:59:53 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,10 +51,25 @@ document.addEventListener('DOMContentLoaded', function() {
         return result;
     }
 
-    
+    function encryptText(text) {
+        const shift = 3;
+        let result = "";
 
+        for (let i = 0; i < text.length; i++) {
+            let char = text.charAt(i);
+            if (char.match(/[a-z]/i)) {
+                let code = text.charCodeAt(i);
+                if (code >= 65 && code <= 90) {
+                    char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+                } else if (code >= 97 && code <= 122) {
+                    char = String.fromCharCode(((code - 97 + shift) % 26) +97);
+                }
+            }
+            result += char;
+        }
 
-    
+        return result;
+    }    
 
     // Adicionar evento ao botão "Copiar"
     copyButton.addEventListener('click', function() {
